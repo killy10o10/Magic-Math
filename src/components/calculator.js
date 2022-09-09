7379ae834496a3f62edbe423c084a4b9071c6552
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-09-09 01:46:10 +0000

    add calculator logic

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from './Button';
+import calculate from '../logic/calculate';
 
 const buttons = [
   'AC',
@@ -24,13 +25,46 @@ const buttons = [
 ];
 
 class Calculator extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    const Target = e.target.textContent;
+    this.setState((state) => calculate(
+      {
+        next: state.next,
+        total: state.total,
+        operation: state.operation,
+      },
+      Target,
+    ));
+  }
+
   render() {
+    const { total, next, operation } = this.state;
     return (
       <div className="calc">
-        <input type="text" placeholder={0} className="calc-input" />
+        <input
+          type="text"
+          placeholder={0}
+          className="calc-input"
+          disabled
+          value={
+            (total === null ? '' : total)
+            + (operation === null ? '' : operation)
+            + (next === null ? '' : next)
+          }
+        />
         <div className="btn-contain">
           {buttons.map((button) => (
-            <Button key={button} btnName={button} />
+            <Button key={button} btnName={button} event={this.handleClick} />
           ))}
         </div>
       </div>
