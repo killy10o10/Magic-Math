2801f39a08d05d8c7dff1898585ae9a55752a2c1
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-09-16 10:28:24 +0100

    did the operate test in __test___/operate.testt.js

--- src/__tests___/operate.test.js
+++ src/__tests___/operate.test.js
@@ -5,4 +5,23 @@ describe('Tests for calculate', () => {
     const plusOperator = operate(2, 3, '+');
     expect(plusOperator).toBe('5');
   });
+  test('Should return the 10-5 = 5', () => {
+    const minus = operate(10, 5, '-');
+    expect(minus).toBe('5');
+  });
+
+  test('Should return the 10 x 5 = 50', () => {
+    const multiply = operate(10, 5, 'x');
+    expect(multiply).toBe('50');
+  });
+
+  test('Should return the 10÷5 = 5', () => {
+    const divide = operate(10, 5, '÷');
+    expect(divide).toBe('2');
+  });
+
+  test('Should return the 10%5 = 0', () => {
+    const modulo = operate(10, 5, '%');
+    expect(modulo).toBe('0');
+  });
 });
